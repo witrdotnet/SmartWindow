9ad39864d5e34d138146f0600e3a67f9eacd6e0d
witrdotnet <witr.net@gmail.com>
2015-06-07 04:40:00 +0200

    screens

--- www/js/index.js
+++ www/js/index.js
@@ -1,78 +1,55 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-
-//app.initialize();
-
-
-var sw = {
-	initialize: function() {
-	},
-	waitForBeacon: function(afterSec) {
-		setTimeout(function(){
-			$.mobile.navigate( "#waitForBeacon", { transition : "fade", info: "info about the #waitForBeacon hash" });
-		}, afterSec);
-	}
-}
-
-$(".btnexit").live("tap", function(event){
-         navigator.notification.confirm(
-             'Do you want to quit', 
-             onConfirmQuit, 
-             'QUIT TITLE', 
-             'OK,Cancel'  
-         );
+$(document).on("pagecreate", "#home", function() {
+	setTimeout(function() {
+		$.mobile.navigate( "#waitForBeacon", { transition : "fade", info: "info about the #waitForBeacon hash" });
+	}, 3000);
 });
 
+$(document).on("pagecreate", "#waitForBeacon", function() {
+	$( "#waitForBeacon" ).on( "tap", function() {
+		$.mobile.navigate( "#loadFromBeacon", { transition : "fade", info: "info about the #loadFromBeacon hash" });
+	});
 
-    function onConfirmQuit(button){
-        if(button == "1"){
-            navigator.app.exitApp(); 
-        }
-    }
+	$(".btnexit").on("tap", function(){
+		navigator.app.exitApp(); 
+	});
+});
+
+$(document).on("pagecreate", "#loadFromBeacon", function() {
+	setTimeout(function() {
+		$.mobile.navigate( "#info1", { transition : "fade", info: "info about the #info1 hash" });
+	}, 3000);
+});
+
+$(document).on("pagecreate", "#info1", function() {
+	$( "#info1" ).on( "tap", function() {
+		$.mobile.navigate( "#info2", { transition : "fade", info: "info about the #info2 hash" });
+	});
+});
+
+$(document).on("pagecreate", "#info2", function() {
+	$( "#info2" ).on( "tap", function() {
+		$.mobile.navigate( "#info3", { transition : "fade", info: "info about the #info3 hash" });
+	});
+});
+
+$(document).on("pagecreate", "#info3", function() {
+	$( "#info3" ).on( "tap", function() {
+		$.mobile.navigate( "#info4", { transition : "fade", info: "info about the #info4 hash" });
+	});
+});
+
+$(document).on("pagecreate", "#info4", function() {
+	$( "#info4" ).on( "tap", function() {
+		$.mobile.navigate( "#endInfos", { transition : "fade", info: "info about the #endInfos hash" });
+	});
+});
+
+$(document).on("pagecreate", "#endInfos", function() {
+	$(".btnYeah").on("tap", function(){
+		$.mobile.navigate( "#waitForBeacon", { transition : "fade", info: "info about the #waitForBeacon hash" });
+	});
+
+	$(".btnNope").on("tap", function(){
+		$.mobile.navigate( "#bye", { transition : "fade", info: "info about the #bye hash" });
+	});
+});
