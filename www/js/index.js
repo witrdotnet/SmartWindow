b097da62e0342bf6cb943a4f6977cdb9e25fca0c
witrdotnet <witr.net@gmail.com>
2015-06-07 05:22:02 +0200

    css

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,4 @@
+
 $(document).on("pagecreate", "#home", function() {
 	setTimeout(function() {
 		$.mobile.navigate( "#waitForBeacon", { transition : "fade", info: "info about the #waitForBeacon hash" });
@@ -21,24 +22,36 @@ $(document).on("pagecreate", "#loadFromBeacon", function() {
 });
 
 $(document).on("pagecreate", "#info1", function() {
+	$(".btnX").on("tap", function(){
+		navigator.app.exitApp(); 
+	});
 	$( "#info1" ).on( "tap", function() {
 		$.mobile.navigate( "#info2", { transition : "fade", info: "info about the #info2 hash" });
 	});
 });
 
 $(document).on("pagecreate", "#info2", function() {
+	$(".btnX").on("tap", function(){
+		navigator.app.exitApp(); 
+	});
 	$( "#info2" ).on( "tap", function() {
 		$.mobile.navigate( "#info3", { transition : "fade", info: "info about the #info3 hash" });
 	});
 });
 
 $(document).on("pagecreate", "#info3", function() {
+	$(".btnX").on("tap", function(){
+		navigator.app.exitApp(); 
+	});
 	$( "#info3" ).on( "tap", function() {
 		$.mobile.navigate( "#info4", { transition : "fade", info: "info about the #info4 hash" });
 	});
 });
 
 $(document).on("pagecreate", "#info4", function() {
+	$(".btnX").on("tap", function(){
+		navigator.app.exitApp(); 
+	});
 	$( "#info4" ).on( "tap", function() {
 		$.mobile.navigate( "#endInfos", { transition : "fade", info: "info about the #endInfos hash" });
 	});
